e0d287236dab314c446b936fc2b29d8869d00a4a
Thom Patterson <ThomPatterson@gmail.com>
2018-04-06 07:55:24 -0500

    Added accordion

--- output/script.js
+++ output/script.js
@@ -1,36 +1,61 @@
 let maxImagesPerHour = 120; //every 30 seconds for worst case scenario of 3600 images
 
-$(document).ready(() => {
+$(document).ready(function() {
 
-  $('.hour h2').click(function() {
+  //setup basic accordion functionality
+  setupAccordionMultiOpen();
 
-    //get a reference to the wrapper for this hour
-    let $hour = $(this).parent();
-
-    //load images if they haven't already been
-    if (!hasLoadedImages($hour)) loadImages($hour);
-
-    //toggle visibility for this hour
-    $(this).siblings('.imageWrapper.loaded').toggle();
+  //setup dynamic image loading
+  $('.hour').click(function() {
+    let $hourWrapper = $(this).parent();
+    if (!hasLoadedImages($hourWrapper)) {
+      console.log('No images loading, starting the process');
+      loadImages($hourWrapper);
+    }
   });
-
-
 
 });
 
-function hasLoadedImages($hour) {
-  return $hour.children('.loaded').length > 0;
+function setupAccordionOneOpen() {
+  //https://codepen.io/brenden/pen/Kwbpyj
+  $('.toggle').click(function(e) {
+    e.preventDefault();
+
+    var $this = $(this);
+
+    if ($this.next().hasClass('show')) {
+      $this.next().removeClass('show');
+      $this.next().slideUp(350);
+    } else {
+      $this.parent().parent().find('li .inner').removeClass('show');
+      $this.parent().parent().find('li .inner').slideUp(350);
+      $this.next().toggleClass('show');
+      $this.next().slideToggle(350);
+    }
+  });
 }
 
-function loadImages($hour) {
-  let $unloadedImages = $hour.children('.notLoaded');
+function setupAccordionMultiOpen() {
+  $('.toggle').click(function(e) {
+    var $this = $(this);
+    $this.next().toggleClass('show');
+    $this.next().slideToggle(350);
+  });
+}
+
+function hasLoadedImages($hourWrapper) {
+  return $hourWrapper.find('.loaded').length > 0;
+}
+
+function loadImages($hourWrapper) {
+  let $unloadedImages = $hourWrapper.find('.notLoaded');
   let numUnloadedImages = $unloadedImages.length;
   if (numUnloadedImages > maxImagesPerHour) {
     let everyNthImage = Math.round(numUnloadedImages / maxImagesPerHour);
     for (let i = 0; i <= numUnloadedImages; i += everyNthImage) {
       loadImage($unloadedImages.eq(i));
     }
-    updateMessage($hour, $hour.children('.loaded').length, numUnloadedImages, everyNthImage);
+    updateMessage($hour, $hourWrapper.find('.loaded').length, numUnloadedImages, everyNthImage);
   } else {
     $unloadedImages.each(function() {
       loadImage($(this));
@@ -51,16 +76,16 @@ function updateMessage($hour, numLoaded, total, everyNth) {
 }
 
 function ordinalSuffixOf(i) {
-    var j = i % 10,
-        k = i % 100;
-    if (j == 1 && k != 11) {
-        return i + "<sup>st</sup>";
-    }
-    if (j == 2 && k != 12) {
-        return i + "<sup>nd</sup>";
-    }
-    if (j == 3 && k != 13) {
-        return i + "<sup>rd</sup>";
-    }
-    return i + "<sup>th</sup>";
+  var j = i % 10,
+    k = i % 100;
+  if (j == 1 && k != 11) {
+    return i + "<sup>st</sup>";
+  }
+  if (j == 2 && k != 12) {
+    return i + "<sup>nd</sup>";
+  }
+  if (j == 3 && k != 13) {
+    return i + "<sup>rd</sup>";
+  }
+  return i + "<sup>th</sup>";
 }
