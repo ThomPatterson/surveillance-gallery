6dc00505043116a02493be0f76ed24c8d860926c
Thom Patterson <ThomPatterson@gmail.com>
2018-06-02 12:38:01 -0500

    removing commented out code

--- index.js
+++ index.js
@@ -11,23 +11,10 @@ let dvrWorkDir = '/Volumes/surveillance-breezeway';
 
 let fileExt = '.jpg';
 
-//also assuming there is only one camera
-let jpgDir = '/001/jpg';
-
 //set to 1 to just get today's snapshots
 //set to 2 to get yesterday and today, etc.
 //set to something like 999999 to get all available days
 let daysToFetch = 2;
-
-//array of filenames to ignore
-let ignoreFiles = ['.DS_Store']
-
-//store jpg paths in multi dimension array, [date][hour][minute] = [paths]
-
-//TODO
-//using hours and minutes (str) as object keys results in sorting where 10, 20, 30, etc come before 00, 01.  Resorted to prefixing time with letter.
-
-
 
 
 function getTemplate() {
@@ -50,44 +37,11 @@ function writeHtml(html) {
 }
 
 
-
-
-/*
-let galleryTemplate;
-getTemplate()
-  .then(templateSrc => {
-    galleryTemplate = Handlebars.compile(templateSrc);
-    return getAvailableDates();
-  })
-  .then(data => {
-    let filteredDates = filterDates(data, daysToFetch);
-    return getAvailableHours(filteredDates);
-  })
-  .then(data => {
-    return getAvailableMinutes(data);
-  })
-  .then(data => {
-    return getAvailableSnapshots(data);
-  }).then(data => {
-    let html = galleryTemplate({
-      date: data
-    });
-    return writeHtml(html);
-  }).then(filePath => {
-    let cmd = 'open -a "Google Chrome" "file://' + __dirname + '/' + filePath + '"';
-    childProc.exec(cmd);
-  }).catch(err => {
-    console.error(err);
-  });
-
-  */
-
-let files = fileFinder(dvrWorkDir, fileExt, daysToFetch);
-
 getTemplate()
   .then(templateSrc => {
     registerHelpers();
     let galleryTemplate = Handlebars.compile(templateSrc);
+    let files = fileFinder(dvrWorkDir, fileExt, daysToFetch);
     let html = galleryTemplate(
       files
     );
